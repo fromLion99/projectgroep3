4e1d7928fbe6d890f6df882c54b928c9b4b87f23
AKEuwijk 0996410 <ariekorneliseuwijk@gmail.com>
2020-02-12 10:55:50 +0100

    Hello World Cinema ipv Hello World

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -6,7 +6,7 @@ namespace cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Cinema!");
         }
     }
 }
