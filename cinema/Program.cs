dde0da0a17dbb2af2b59a3126e3b7986473de844
NielsVLD <nielskrommenhoek@hotmail.nl>
2020-03-10 14:46:14 +0100

    Super goeie update

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -7,6 +7,7 @@ namespace cinema
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Cinema!");
+            Console.WriteLine("Dit project word de bom!");
         }
     }
 }
