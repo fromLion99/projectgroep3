7e924746853e09ade506f885b5af9c4893fb307a
Wessel van Leeuwen <wesselvleeuwen@outlook.com>
2020-03-25 15:43:17 +0100

    Taakverdeling update 2

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -7,11 +7,11 @@ namespace cinema
         static void Main(string[] args)
         {
             //Taakverdeling:
-                //Wessel: Toevoegen van films en zalen en het weergeven van films en zalen en support bieden.
+                //Wessel: Toevoegen van films en zalen en het weergeven van films en zalen en support bieden. Bepalen wanneer er code in de master branch gaat.
                 //Roby: Welkomsscherm
                 //Niels: Zoekfunctie
                 //Dennis: Inlogscherm
-                //Korneel: updateMovie() 
+                //Korneel: Film informatie opvraag
         }
     }
 }
