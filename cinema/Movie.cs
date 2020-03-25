75af36f480220d3651f49bee3cafd497ec00db10
Wessel van Leeuwen <wesselvleeuwen@outlook.com>
2020-03-25 16:00:01 +0100

    Movie & Room class added

---
+++ cinema/Movie.cs
@@ -0,0 +1,15 @@
+namespace cinema
+{
+    public class Movie
+    {
+        //Properties Movie
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public string Time {get; set;}
+        public string Date {get; set;}
+        public int Zaal {get; set;}
+        public bool Imax {get; set;}
+        public bool DrieD {get; set;}
+    }
+}
---
+++ cinema/Room.cs
@@ -0,0 +1,12 @@
+namespace cinema
+{
+    public class Room
+    {
+        //Properties Room
+        public int Id {get; set;}
+        public int RoomNumber {get; set;}
+        public int CountChair {get; set;}
+        public int Row {get; set;}
+        public int Chair {get; set;}
+    }
+}
