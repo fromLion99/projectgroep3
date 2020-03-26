6431eb42e399043ed76f4d2e5f5ab81ef2987173
Rolfazay <58993580+Rolfazay@users.noreply.github.com>
2020-03-26 14:41:30 +0100

    ascii

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -18,32 +18,65 @@ namespace cinema
             Movie m = new Movie();
             Room r = new Room();
 
-            string action0, action1, action2 = "";
-            Console.WriteLine("\nWhat will you do? Enter M for movies or R for rooms.");
-            action0 = Console.ReadLine();
-            if(action0 == "M" || action0 == "m"){
-                Console.WriteLine("For view all movies enter V. Add a movie enter A.");
-                action1 = Console.ReadLine();
-                if(action1 == "A" || action1 == "a"){
-                    m.addMovie();
+            string titleScherm, action3 = "";
+            
+ var arr = new[]
+            {
+                    @"   __      __        _                              _             ____  _                                   ",
+                    @"   \ \    / /  ___  | |  __   ___   _ __    ___    | |_   ___    |_  / (_)  ___   _ _    ___   _ __    __ _ ",
+                    @"    \ \/\/ /  / -_) | | / _| / _ \ | '  \  / -_)   |  _| / _ \    / /  | | / -_) | ' \  / -_) | '  \  / _` |", 
+                    @"     \_/\_/   \___| |_| \__| \___/ |_|_|_| \___|    \__| \___/   /___| |_| \___| |_||_| \___| |_|_|_| \__,_|", 
+                    @"                                                                                                            ", 
+                    @"   Press any key to start                                                                                     ",           
+            };
+            Console.WriteLine("\n\n");
+            foreach(string line in arr )
+                Console.WriteLine(line);
+            Console.ReadKey();
+                Console.WriteLine("Are you a customer?, Yes or No?");
+                titleScherm = Console.ReadLine();
+                if(titleScherm == "Yes" || titleScherm == "y" || titleScherm == "Y"){
+                    Console.WriteLine("Enter M to view all movies or L to login into your account");
+                    action3 = Console.ReadLine();
+                    if(action3 == "M"){
+                        m.viewMovie();
+                    }
+                    if (action3 == "L"){
+                        Console.WriteLine("This feature does not exist yet, sorry :(");
+                    }  
                 }
-                else{
-                    m.viewMovie();
-                }
-            }
-            if(action0 == "R" || action0 == "r"){
-                Console.WriteLine("For view all rooms enter V. Add a room enter A.");
-                action2 = Console.ReadLine();
-                if(action2 == "A" || action2 == "a"){
-                    r.addRoom();
-                }
-                else{
-                    r.viewRoom();
-                }
-            }
-            else{
-                Console.WriteLine("Unknown command. Please run again this application.\nIf this message keeps showing on you probily do not understand how this application works.");
-            }
+                else {
+                    Console.WriteLine("Sorry this feature does not exist yet, sorry :(");
+                } 
         }
     }
-}
+}                                                                                           
+                // Console.WriteLine("\nWhat will you do? Enter M for movies or R for rooms.");
+                // action0 = Console.ReadLine();
+                // if(action0 == "M" || action0 == "m"){
+                // Console.WriteLine("For view all movies enter V. Add a movie enter A.");
+                // action1 = Console.ReadLine();
+                // if(action1 == "A" || action1 == "a"){
+                //     m.addMovie();
+                // }
+                // else{
+                //     m.viewMovie();
+                // }
+                
+            // }
+            // if(action0 == "R" || action0 == "r"){
+            //     Console.WriteLine("For view all rooms enter V. Add a room enter A.");
+            //     action2 = Console.ReadLine();
+            //     if(action2 == "A" || action2 == "a"){
+            //         r.addRoom();
+            //     }
+            //     else{
+            //         r.viewRoom();
+            //     }
+            // }
+            // else{
+            //     Console.WriteLine("Unknown command. Please run again this application.\nIf this message keeps showing on you probily do not understand how this application works.");
+            // }
+        // }
+    // }
+// }
