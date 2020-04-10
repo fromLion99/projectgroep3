aa3c7a074dd77ff1a847de5a598e72c8978a3573
Wessel van Leeuwen <wesselvleeuwen@outlook.com>
2020-04-10 15:26:49 +0200

    Updated Program.cs

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -14,12 +14,55 @@ namespace cinema
             Customer c = new Customer();
             Reservation R = new Reservation();
                   
-            string start, showMovies, showRooms, addMovies, rooms, login = "";
+            string start, account, showMovies, showRooms, addMovies, rooms, login = "";
             
             p.startScreen();
 
             bool gotoMovies = false;
             bool gotostart = false;
+
+            Console.WriteLine("After pressing a key hit enter to go further in the program.\nWill you see movies press M. Will you login or make an account press L.");
+            start = Console.ReadLine();
+            if(start == "M" || start == "m")
+            {
+                Console.WriteLine("Available movies, if you wat to login press L");
+                login = Console.ReadLine();
+                m.viewMovie();
+                moviesStart:
+                gotoMovies = false;
+                if (login == "L"|| login == "l" ){
+                    Console.WriteLine("Do you already have an account? Yes or No?");
+                    login = Console.ReadLine();
+                    if (login == "yes"|| login == "Yes" || login == "y"|| login == "Y" )
+                    {
+                        l.signIn();
+                        Console.WriteLine("Login successful, Press M for movies");
+                        showMovies = Console.ReadLine();
+                        if(showMovies == "m" || showMovies == "M")
+                        {
+                            m.viewMovie();
+                            gotoMovies = true;
+                            if(gotoMovies)
+                            {
+                                goto moviesStart;
+                            }
+                        }
+                    }
+                }
+            }
+            if(start == "L" || start == "l")
+            {
+                Console.WriteLine("Press L for log in. Press C to creare an account.");
+                account = Console.ReadLine();
+                if(account == "L" || account == "l")
+                {
+                    l.signIn();
+                }
+                if(account == "C" || account == "c")
+                {
+                    c.addCustomer();
+                }
+            }
         }
 
         public void startScreen(){
