242265ce1faf9d0509198e0f3f79fd7168b681da
fromLion99 <48238522+fromLion99@users.noreply.github.com>
2020-05-03 10:59:21 +0200

    Added the searchMovie()

--- cinema/Program.cs
+++ cinema/Program.cs
@@ -54,7 +54,7 @@ namespace cinema
         {
             string guestAction = "";
 
-            Console.WriteLine("Do you wan to create an account enter C. Do you want to login enter L. Do you want to login as Employee press E. Do you want to see all movies press M. Do you want to close the program press Q.");
+            Console.WriteLine("Do you wan to create an account enter C. Do you want to login enter L. Do you want to login as Employee press E. Do you want to see all movies press M. Do you want to search trough the movies press S. Do you want to close the program press Q.");
             guestAction = Console.ReadLine();
 
             switch (guestAction)
@@ -71,6 +71,9 @@ namespace cinema
                 case "M": case "m":
                     Movie.viewMovie();
                     break;
+                case "S": case "s":
+                    Search.searchMovie();
+                    break;
                 case "Q": case "q":
                     shutDown();
                     break;
@@ -83,7 +86,7 @@ namespace cinema
 
             startCustomer:
 
-            Console.WriteLine("Will you see movies press M. Will you make a reservation press R. If you want to logout and/or close the program press Q.");
+            Console.WriteLine("Will you see movies press M. Will you make a reservation press R. Will you search trough the movies press S. If you want to logout and/or close the program press Q.");
             customerAction = Console.ReadLine();
 
             switch (customerAction)
@@ -95,6 +98,9 @@ namespace cinema
                     Reservation.addReservation();
                     Reservation.addSeatReservation();
                     goto startCustomer;
+                case "S": case "s":
+                    Search.searchMovie();
+                    break;
                 case "Q": case "q":
                     shutDown();
                     break;
